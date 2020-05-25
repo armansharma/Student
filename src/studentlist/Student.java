eabe8375e7ceebdfc58deea33d0629d3a2b590c3
Arman Sharma <sarman302@gmail.com>
2020-05-25 15:41:46 -0400

    Revert Second commit

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -9,26 +9,6 @@ public class Student {
     
     private String name;
 
-    private String email;
-
-    /**
-     * Get the value of email
-     *
-     * @return the value of email
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * Set the value of email
-     *
-     * @param email new value of email
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public Student(String name) {
         this.name = name;
     }
